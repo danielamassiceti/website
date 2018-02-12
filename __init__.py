0b2846b42c78e1d584dc970c3eeb3c27d486ca1b
Daniela Massiceti <daniela.massiceti@gmail.com>
2018-02-12 21:33:46 +0000

    make separate server folder

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ from research import flipdial as fd
 app = Flask(__name__)
 app.template_folder = os.path.join(app.root_path, 'templates')
 
-cv = 'danielamassiceti_cv_nov2017.pdf'
+cv = 'danielamassiceti_cv_jan2018.pdf'
 
 app.register_blueprint(_rfs_vs_nns, url_prefix='/research/rfs_vs_nns')
 app.register_blueprint(_bottom_up_top_down, url_prefix='/research/bottom_up_top_down')
@@ -16,7 +16,7 @@ app.register_blueprint(_bottom_up_top_down, url_prefix='/research/bottom_up_top_
 sv.set_cv(cv)
 app.register_blueprint(sv._stereosonic_vision, url_prefix='/research/stereosonic_vision')
 
-fd.initialise_model()
+#fd.initialise_model()
 fd.set_cv(cv)
 app.register_blueprint(fd._flipdial, url_prefix='/research/flipdial')
 
--- research/flipdial/__init__.py
+++ research/flipdial/__init__.py
@@ -1,26 +1,32 @@
 from flask import Blueprint, request, render_template, url_for, redirect
-import os, uuid, pickle, itertools
-import visdial.visdial as visdial
+import os, uuid, pickle, itertools, requests, json
 
-global maxchatlen, chat, imglist, caps, vd, img_to_load, cv
+global maxchatlen, chat, imglist, caps, img_to_load, cv, current_img_path, current_cap
 maxchatlen = 10
 chat = []
     
 _flipdial = Blueprint('flipdial', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
+imglist = os.listdir(os.path.join(_flipdial.static_folder, 'thumbnails'))
+caps = pickle.load(open(os.path.join(_flipdial.static_folder, 'cap_dict'), 'rb'))
+
+vd_server = 'http://129.67.94.221'
+vd_port = '26736'
 
 def set_cv(cv_path):
     global cv
     cv = cv_path
 
-def initialise_model():
-    global vd, caps, imglist
-    imglist = os.listdir(os.path.join(_flipdial.static_folder, 'thumbnails'))
-    caps = pickle.load(open(os.path.join(_flipdial.static_folder, 'cap_dict'), 'rb'))
-    vd = visdial.VisDial(_flipdial.static_folder)
-
-
-def root_dir():  # pragma: no cover
-    return os.path.abspath(os.path.dirname(__file__))
+def get_vd_url(t, question, img_path, caption, chat):
+    vd_url = vd_server + ':' + vd_port + '/get_answer?'
+    vd_url += 't=' + str(t)
+    vd_url += '&question=' + question
+    vd_url += '&img_path=' + img_path
+    vd_url += '&caption=' + caption
+    vd_url += '&history=' + json.dumps(chat)
+    return vd_url
+    
+def ravel_answers(answers):
+    return answers.split('**')
 
 @_flipdial.route('/', methods=['GET', 'POST'])
 def home():
@@ -28,76 +34,69 @@ def home():
 
 @_flipdial.route('/demo', methods=['GET', 'POST'])
 def demo():
-    global maxchatlen, chat, imglist, caps, vd, img_to_load, cv
+    global maxchatlen, chat, imglist, caps, img_to_load, cv, current_img_path, current_cap
     default_img = 'COCO_val2014_000000524382.jpg'
 
-    if not vd:
-        initialise_model()
-    
     if request.method=='POST' and len(chat) < maxchatlen :
         if 'reset' in request.form.keys():
             chat = []
             return render_template('flipdial_demo.html', chat=chat, scroll='demo', autofocus='autofocus', questiontext='enter your question...', imgs=imglist, img_to_load=img_to_load, cv=cv)
             
         elif 'question' in request.form.keys():
-	    return 'h1'
             question = request.form['question']
             t = len(chat)*2
 
-            if vd.check_img and vd.check_cap: #if img and cap features are loaded
-                # get answer
-                answer = vd.get_answer(t, question)
-                answers = list(itertools.chain(*answer))
-            else:
-                return 'error'
+            # get answer by pinging server where flipdial model is loaded
+            vd_url = get_vd_url(t, question, img_path, caption, chat)
+            try: 
+                answer = requests.get(get_vd_url(t, question, img_path, caption, chat)) # get answer
+                answers = ravel_answers(answer.text)
+            except requests.exceptions.RequestException as e:  # This is the correct syntax
+                return e
 
             item = (question, answers)
             chat.append(item)
-            #img_to_load = ( url_for('.static', filename=os.path.join('images', default_img)), caps[default_img] )
             return render_template('flipdial_demo.html', chat=chat, scroll='demo', autofocus='autofocus', questiontext='', imgs=imglist, img_to_load=img_to_load, cv=cv)
-        elif 'img_to_load' in request.form.keys():
+        
+        elif 'img_to_load' in request.form.keys(): # select image from panel
             
             # empty chat
             chat = []
             
             # get image selected
-            #imgname = request.form['img_to_load']
-            #img_path = os.path.join(_flipdial.static_folder, 'images', imgname)
-            #img_to_load = ( url_for('.static', filename=os.path.join('images', imgname)), '' )
+            imgname = request.form['img_to_load']
+            current_cap = caps[imgname]
+            current_img_path = os.path.join(_flipdial.static_folder, 'images', imgname)
+            img_to_load = ( url_for('.static', filename=os.path.join('images', imgname)), '' )
 
-            # set new image and caption features
-            #vd.reset(img_path, caps[imgname])
-	    return 'pass' 
-            #return render_template('flipdial_demo.html', chat=chat, scroll='demo', questiontext='enter your question...', imgs=imglist, img_to_load=img_to_load, cv=cv)
+            return render_template('flipdial_demo.html', chat=chat, scroll='demo', questiontext='enter your question...', imgs=imglist, img_to_load=img_to_load, cv=cv)
 
-        elif 'upload_img' in request.files.keys():
-            return 'h2'
+        elif 'upload_img' in request.files.keys(): # upload own image
             
             # empty chat
             chat = []
 
             # get image uploaded and save locally with unique filename
             img_file = request.files['upload_img']
+            current_cap = 'PAD EOS'
             unique_filename = str(uuid.uuid4())
-            img_path = os.path.join(_flipdial.static_folder, 'uploaded_images', unique_filename)
-            while os.path.exists( img_path ):
+            current_img_path = os.path.join(_flipdial.static_folder, 'uploaded_images', unique_filename)
+            while os.path.exists( current_img_path ):
                 unique_filename = str(uuid.uuid4())
-                img_path = os.path.join(_flipdial.static_folder, 'uploaded_images', unique_filename)
-            img_file.save(img_path)
+                current_img_path = os.path.join(_flipdial.static_folder, 'uploaded_images', unique_filename)
+            img_file.save(current_img_path)
             img_to_load = ( url_for('.static', filename=os.path.join('uploaded_images', unique_filename)), '' )
 
-            # set new image and caption features
-            vd.reset(img_path, 'PAD EOS')
             
             return render_template('flipdial_demo.html', chat=chat, scroll='demo', questiontext='enter your question...', imgs=imglist, img_to_load=img_to_load, cv=cv)
-    else:
-        return 'h3' 
+    else: #standard page load
+
         # empty chat
         chat = []
-
-        # set new image and caption features
-        img_path = os.path.join(_flipdial.static_folder,'images', default_img)
-        vd.reset(img_path, caps[default_img])
+        
+        # set image and caption to default options
+        current_img_path = os.path.join(_flipdial.static_folder,'images', default_img)
+        current_cap = caps[default_img]
         
         img_to_load = ( url_for('.static', filename=os.path.join('images', default_img)), '' )
         return render_template('flipdial_demo.html', chat=chat, questiontext="enter your question...", imgs=imglist, img_to_load=img_to_load, cv=cv )
