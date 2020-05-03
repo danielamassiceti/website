5371002c728b73129aa39e5f78ef56835db4888f
Daniela Massiceti <daniela.massiceti@gmail.com>
2020-05-03 21:16:20 +0100

    updated cv

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-cv = 'danielamassiceti_cv_sept2019.pdf'
+cv = 'danielamassiceti_cv_may2020.pdf'
 mykeywords = 'daniela massiceti, university of oxford, phd student, phil torr, torr vision group, computer vision, oxford, microsoft research, cambridge, orbit'
 jobtitle = 'Researcher, Microsoft Research Cambridge'
 vd_host = '129.67.94.52'
